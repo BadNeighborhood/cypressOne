e94fce9d2a956e060cbd2fc7547924eff2aa1474
Dmytro_Kuznietsov <kdvgym@gmail.com>
2021-08-09 14:36:32 +0300

    signupcypress

---
+++ cypress/integration/signup.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('', () => {
+    let user;
+    before('', () => {
+            cy.visit('http://localhost:1667/#/');
+            cy.task("freshUser").then((object) => {
+                user = object;
+            });
+        });
+        it('', () => {
+            cy.get(':nth-child(3) > .nav-link').click();
+            cy.get(':nth-child(1) > .form-control').type(user.username);
+            cy.get(':nth-child(2) > .form-control').type(user.email);
+            cy.get(':nth-child(3) > .form-control').type(user.password);
+            cy.get('.btn').click();
+            cy.get('.swal-button').click();
+            cy.get(':nth-child(4) > .nav-link').contains(user.username).should('exist');
+        });
+    });
