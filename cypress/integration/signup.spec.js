12347f643ed4a35e8fed99c447e18745bf847f1f
Dmytro_Kuznietsov <kdvgym@gmail.com>
2021-08-09 15:00:29 +0300

    swalModalCheckAdd

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -14,6 +14,7 @@ describe('', () => {
             cy.get(':nth-child(2) > .form-control').type(user.email);
             cy.get(':nth-child(3) > .form-control').type(user.password);
             cy.get('.btn').click();
+            cy.get('.swal-modal').should('contain', 'Your registration was successful!');
             cy.get('.swal-button').click();
             cy.get(':nth-child(4) > .nav-link').contains(user.username).should('exist');
         });
